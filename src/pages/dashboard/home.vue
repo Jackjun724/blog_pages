f8cda693d8350d012b38dbb8de93bc605a73b422
Jack <jackjun0724@163.com>
2019-10-02 17:42:16 +0800

    前台速度优化

--- src/pages/dashboard/home.vue
+++ src/pages/dashboard/home.vue
@@ -107,7 +107,16 @@
 
 <script>
   import wordCloud from 'wordcloud'
-  import ECharts from 'echarts'
+  import ECharts from 'echarts/lib/echarts'
+  // 引入折线图/柱状图等组件
+  import 'echarts/lib/chart/line'
+  import 'echarts/lib/chart/bar'
+  import 'echarts/lib/chart/radar'
+  // 引入提示框和title组件，图例
+  import 'echarts/lib/component/tooltip'
+  import 'echarts/lib/component/title'
+  import 'echarts/lib/component/legend'
+  import 'echarts/lib/component/legendScroll'
 
   export default {
     name: 'dashboard',
