8b9feb4a4fd31aca57a1b7943602adcabf14a51d
王俊杰 <jack@retzero.com>
2019-10-19 22:11:12 +0800

    fix style

--- src/api/modules/system.js
+++ src/api/modules/system.js
@@ -3,3 +3,7 @@ import Axios from 'axios'
 export function Submit(param){
   return Axios.get('system/unknown',param)
 }
+
+export function GoodsList(){
+  return Axios.get('system/get-goods')
+}
--- src/pages/setting/setting.vue
+++ src/pages/setting/setting.vue
@@ -6,12 +6,16 @@
           模拟请求工具
         </template>
         <label>
-          行
-          <el-input-number class="number-input" :min="1" :max="10" v-model="row" label="行"></el-input-number>
+          GOODS
+          <el-select v-model="id" class="number-input">
+            <el-option v-for="item in goods" :value="item.goods_id" :key="item.goods_id">
+              {{item.goods_name}}
+            </el-option>
+          </el-select>
         </label>
         <label>
-          列
-          <el-input-number class="number-input" :min="1" :max="10" v-model="col" label="列"></el-input-number>
+          PRICE
+          <el-input-number class="number-input" :min="0.1" :max="10" :step="0.1" v-model="price"></el-input-number>
         </label>
         <el-button @click="submit" type="primary">提交</el-button>
         <a :href="href" target="_blank" v-show="href" id="target-buy"><el-button type="danger">Go</el-button></a>
@@ -21,22 +25,23 @@
 </template>
 
 <script>
-  import {Submit} from '@/api/modules/system'
+  import {Submit,GoodsList} from '@/api/modules/system'
 
   export default {
     data () {
       return {
-        row: null,
-        col: null,
-        href: ''
+        id: null,
+        price: 2.0,
+        href: '',
+        goods:[]
       }
     },
     methods:{
       submit(){
         let param = {
           params: {
-            col:this.col,
-            row:this.row
+            id:this.id,
+            price:this.price
           }
         }
         let _this = this
@@ -44,6 +49,12 @@
           _this.href = resp.data.data
         })
       }
+    },
+    created () {
+      let _this = this
+      GoodsList().then(resp=>{
+        _this.goods = resp.data.data
+      })
     }
   }
 </script>
